b824ee60eab85db0d9d86f6ace03f94ca687d73d
Gernot Ohner <gernot.ohner@locatee.ch>
2023-08-15 10:14:43 -0600

    Improve data.sql and schema.sql

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,13 +1,14 @@
 
 INSERT INTO tags(id, tag) VALUES (gen_random_uuid(), 'foo');
 
-INSERT INTO users(id, email, password, username, created_at, updated_at, bio, image)
+INSERT INTO users(id, email, token, username, created_at, updated_at, bio, image)
 VALUES (gen_random_uuid(),
         'gernot.ohner@gmail.com',
-        'hashed password',
+        '{noop}password',
         'gernot',
         now(),
         now(),
         'I work at stateless',
         'https://avatars1.githubusercontent.com/u/1024025?s=460&v=4'
-        );
+        )
+ON CONFLICT DO NOTHING;
--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -8,8 +8,8 @@ CREATE TABLE IF NOT EXISTS users
 (
     id UUID PRIMARY KEY,
     username VARCHAR NOT NULL,
-    password VARCHAR NOT NULL,
-    email VARCHAR NOT NULL,
+    token VARCHAR NOT NULL,
+    email VARCHAR NOT NULL UNIQUE ,
     created_at TIMESTAMP NOT NULL DEFAULT NOW(),
     updated_at TIMESTAMP NOT NULL DEFAULT NOW(),
     bio VARCHAR,
