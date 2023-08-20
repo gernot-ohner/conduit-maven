8890e1bd81f47c8b3169911fc8a4510ba2b8df58
Gernot Ohner <gernot.ohner@locatee.ch>
2023-08-20 11:19:11 -0600

    Update schema.sql

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,28 +1,29 @@
 CREATE TABLE IF NOT EXISTS tags
 (
-    id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
-    tag VARCHAR NOT NULL UNIQUE
+    id  UUID    NOT NULL DEFAULT gen_random_uuid(),
+    tag VARCHAR NOT NULL UNIQUE,
+    PRIMARY KEY (id)
 );
 
 CREATE TABLE IF NOT EXISTS users
 (
-    id UUID NOT NULL DEFAULT gen_random_uuid(),
-    username VARCHAR NOT NULL,
-    token VARCHAR NOT NULL,
-    email VARCHAR NOT NULL UNIQUE ,
+    id         UUID      NOT NULL DEFAULT gen_random_uuid(),
+    username   VARCHAR   NOT NULL UNIQUE,
+    password   VARCHAR   NOT NULL,
+    email      VARCHAR   NOT NULL UNIQUE,
     created_at TIMESTAMP NOT NULL DEFAULT NOW(),
     updated_at TIMESTAMP NOT NULL DEFAULT NOW(),
-    bio VARCHAR,
-    image VARCHAR,
+    bio        VARCHAR,
+    image      VARCHAR,
     PRIMARY KEY (id)
 );
 
 CREATE TABLE IF NOT EXISTS user_follower_relation
 (
-    id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
-    user_id UUID NOT NULL,
-    follower_id UUID NOT NULL,
-    created_at TIMESTAMP NOT NULL DEFAULT NOW(),
+    id          UUID PRIMARY KEY   DEFAULT gen_random_uuid(),
+    user_id     UUID      NOT NULL,
+    follower_id UUID      NOT NULL,
+    created_at  TIMESTAMP NOT NULL DEFAULT NOW(),
     FOREIGN KEY (user_id) REFERENCES users (id),
     FOREIGN KEY (follower_id) REFERENCES users (id)
 );
